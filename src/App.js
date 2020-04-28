7ebe026e1ec9c7648d7a97a97e63707936cd67e5
Narender Singh <narender2031@gmail.com>
2020-04-29 02:09:45 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './heder.styles.scss';
+
+const Header = () => {
+	return (
+		<div className="header">
+			<Link to="/">
+				<CrownLogo className="logo" />
+			</Link>
+			<div className="options">
+				<Link className="option" to="/shop">
+					SHOP
+				</Link>
+				<Link className="option" to="/contact">
+					CONTACT
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
